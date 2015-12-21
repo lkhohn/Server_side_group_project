c42430f14a5eb8e7e0e9e56c73b065193315ac2c
LKHohn <lkhohn@gmail.com>
2015-12-21 10:54:29 -0700

    committing updated routes

--- routes/index.js
+++ routes/index.js
@@ -4,50 +4,74 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-// landing page
-  res.render('index', { title: 'Express' });
-
+  // landing page
+  res.render('./index', {
+    linkHome: '/index',
+    linkApt: '/aptSch',
+    linkProfile: '/profile',
+    linkPref: '/pref',
+    linkLogout: '/logout'
+  });
 });
 
-router.post('/', function(req, res, next){
+router.post('/', function(req, res, next) {
   // for login or registration
   var input = req.body.textarea;
 });
 
-router.get('/login', function(req, res, next){
+router.get('/login', function(req, res, next) {
   // for login
-<<<<<<< HEAD
-});
-
-router.get('/signup', function(req, res, next){
-  //for the registration page
-=======
->>>>>>> dev
-});
-
-router.get('/signup', function(req, res, next){
-  // for registration page
-});
-
-router.get('/usrhome', function(req, res, next){
-// home page after login in/registration
-res.render('index', {
-    linkHome:'/home',
-    linkApt: '/apt',
+  res.render('./login', {
+    linkHome: '/index',
+    linkApt: '/aptSch',
     linkProfile: '/profile',
-    linkPref:'/preferences',
-    linkLogout:'/logout'
- });
-
+    linkPref: '/pref',
+    linkLogout: '/logout'
+  });
 });
 
-
-router.get('/pref', function(req, res, next){
-// preferences set up
+router.get('/signup', function(req, res, next) {
+  //for the registration page
+  res.render('./signup', {
+    linkHome: '/index',
+    linkApt: '/aptSch',
+    linkProfile: '/profile',
+    linkPref: '/pref',
+    linkLogout: '/logout'
+  });
 });
 
-router.get('/aptSch', function(req, res, next){
-// appoint set up
+router.get('/usrhome', function(req, res, next) {
+  // home page after login in/registration
+  res.render('./usrhome', {
+    linkHome: '/index',
+    linkApt: '/aptSch',
+    linkProfile: '/profile',
+    linkPref: '/pref',
+    linkLogout: '/logout'
+  });
+});
+
+router.get('/pref', function(req, res, next) {
+  // preferences set up
+  res.render('./pref', {
+    linkHome: '/index',
+    linkApt: '/aptSch',
+    linkProfile: '/profile',
+    linkPref: '/pref',
+    linkLogout: '/logout'
+  });
+});
+
+router.get('/aptSch', function(req, res, next) {
+  // appoint set up
+  res.render('./aptSch', {
+    linkHome: '/index',
+    linkApt: '/aptSch',
+    linkProfile: '/profile',
+    linkPref: '/pref',
+    linkLogout: '/logout'
+  });
 });
 
 
